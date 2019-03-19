8b0684877328af6a5a900b1b82ed1d5017d7fe3d
Daniel M <mcmanimoncoding@gmail.com>
2019-03-18 17:58:26 -0700

    commit

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -30,47 +30,48 @@ const triviaQuestions = [{
 
 var right = 0;
 var wrong = 0;
-var time = 10;
+var time = 120;
 
 
 
-
-$("#start-btn").on("click", function () {
-    console.log("Quiz Start");
-    $("#end-button-disp").html("<button type='submit' class='uk-button-large' id='submit-button1'>HIT THIS BUTTON WHEN COMPLETE!!!</button>");
-    $("#scorecard").empty();
-    $("#scorecard").append("Number Right: " + right);
-    $("#scorecard").append("<br> Number Wrong: " + wrong);
-    $("#scorecard").append("<br> <div class='py-5' id='timer'> <h2> Time Remaining <hr> <h3><div id='time'> </h3>");
-    timeRemaining = setInterval(timer, 1000),
-
-    $("#button-container").remove();
-    for (var i = 0; i < triviaQuestions.length; i++) {
+    $("#start-btn").on("click", function () {
+        console.log("Quiz Start");
+        $("#end-button-disp").html("<button type='submit' class='uk-button-large p-4 m-4 uk-button-primary' id='submit-button1'>HIT THIS BUTTON WHEN COMPLETE!!!</button>");
+        $("#scorecard").empty();
+        $("#scorecard").append("Number Right: " + right);
+        $("#scorecard").append("<br> Number Wrong: " + wrong);
+        $("#scorecard").append("<br> <div class='py-5' id='timer'> <h2> Time Remaining <hr> <h3><div id='time'> </h3>");
+        timeRemaining = setInterval(timer, 1000),
         
-        $("#question-disp").append("<div class=' row question' data-toggle='buttons'><h3>" + triviaQuestions[i].question + "</h3><br></div>")
-        for (var k = 0; k < triviaQuestions[i].answers.length; k++) {
-            $("#question-disp").append("<button class='mx-1 btn btn-secondary'> <input type='radio' checked autocomplete='off' data-toggle='button' aria-pressed='false' auto name='" + i + "' value='" + triviaQuestions[i].answers[k] + "'>" + triviaQuestions[i].answers[k]+"</button>")
+        $("#button-container").remove();
+        for (var i = 0; i < triviaQuestions.length; i++) {
+            
+            $("#question-disp").append("<div class=' row question' data-toggle='buttons'><h3>" + triviaQuestions[i].question + "</h3><br></div>")
+            for (var k = 0; k < triviaQuestions[i].answers.length; k++) {
+                $("#question-disp").append("<button class='mx-1 btn btn-secondary'> <input type='radio' checked autocomplete='off' data-toggle='button' aria-pressed='false' auto name='" + i + "' id='" + triviaQuestions[i].answers[k] + "'>" +" " + triviaQuestions[i].answers[k]+"</button>")
+            }
+        }
+    })
+    
+    $("body").on("click", ".btn", function(){
+        console.log(this);
+    })
+    
+    
+    
+    function timer() {
+        time--;
+        $("#time").html(time+" Seconds... no pressure...");
+        if (time <= 0) {
+            stop();
+            console.log("Time!")
+            // alert("Time!");
+            check();
+            $("#time").html("Times Up!")
         }
     }
-})
-
-
-
-
-
-function timer() {
-    time--;
-    $("#time").html(time+" Seconds... no pressure...");
-    if (time <= 0) {
-        stop();
-        console.log("Time!")
-        // alert("Time!");
-        check();
-        $("#time").html("Times Up!")
-    }
-}
-
-function stop(){
+    
+    function stop(){
     clearInterval(timer);
 }
 
